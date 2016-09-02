c9be50954cddd8393e5390f870a6329823e3dc3e
Varun Ved <varunve@zillowgroup.com>
2016-09-01 20:18:33 -0700

    cleaned up brandon's code, now writes to a json file, also created dependency management with pip

--- toonsWebScraper.py
+++ toonsWebScraper.py
@@ -1,51 +1,66 @@
+#!/usr/bin/env python
+
 from bs4 import BeautifulSoup
-import urllib.request
+import requests
 import re
+import lxml
+import json
 
 def parseOneEpisodeSoup(episodeSoup):
-    episode = {}
-    episode["Title"] = episodeSoup[0].get_text()
-    episode["Series"] = episodeSoup[1].get_text()
-    myList = str(episodeSoup[2]).split("<br/>")
-    animators = []
-    for a in myList:
-        animators.append(re.sub('<[^<]+?>', '',a).replace(",","").replace("\n",""))
-    episode["Animator"] = animators
+  episode = {}
+  episode["Title"] = episodeSoup[0].get_text()
+  episode["Series"] = episodeSoup[1].get_text()
+  myList = str(episodeSoup[2]).split("<br/>")
+  animators = []
+  for a in myList:
+      animators.append(re.sub('<[^<]+?>', '',a).replace(",","").replace("\n",""))
+  episode["Animator"] = animators
 
-    myList = str(episodeSoup[3]).split("<br/>")
-    characters = []
-    for a in myList:
-        characters.append(re.sub('<[^<]+?>', '',a).replace(",","").replace("\n",""))
-    episode["Character"] = characters
-    episode["ID"] = episodeSoup[4].get_text()
-    episode["Date"] = episodeSoup[5].get_text()
-    return(episode)
+  myList = str(episodeSoup[3]).split("<br/>")
+  characters = []
+  for a in myList:
+      characters.append(re.sub('<[^<]+?>', '',a).replace(",","").replace("\n",""))
+  episode["Character"] = characters
+  episode["ID"] = episodeSoup[4].get_text()
+  episode["Date"] = episodeSoup[5].get_text()
+  return(episode)
 
 def parseOneYearSoup(yearSoup):
-    allEpisodesList = yearSoup.find_all("tr")[1:]
-    yearList = []
-    for episodeSoup in allEpisodesList:
-        yearList.append(parseOneEpisodeSoup(episodeSoup.find_all("td")))
-    return(yearList)
+  allEpisodesList = yearSoup.find_all("tr")[1:]
+  yearList = []
+  for episodeSoup in allEpisodesList:
+      yearList.append(parseOneEpisodeSoup(episodeSoup.find_all("td")))
+  return(yearList)
 
 def parseOneWikipediaPageSoup(wikipediaSoup):
-    allYearsList = wikipediaSoup.find_all("table", class_="wikitable sortable")
-    pageList = []
-    for yearSoup in allYearsList:
-        pageList.extend(parseOneYearSoup(yearSoup))
-    return(pageList)
+  allYearsList = wikipediaSoup.find_all("table", class_="wikitable sortable")
+  pageList = []
+  for yearSoup in allYearsList:
+      pageList.extend(parseOneYearSoup(yearSoup))
+  return(pageList)
+
+def pythonJsonWriter(jsonIn):
+  with open('data.json', 'w') as outfile:
+    json.dump(jsonIn, outfile)
 
 
 wikipediaPages =["https://en.wikipedia.org/wiki/Looney_Tunes_and_Merrie_Melodies_filmography_(1929-39)",
-                 "https://en.wikipedia.org/wiki/Looney_Tunes_and_Merrie_Melodies_filmography_(1940-49)",
-                 "https://en.wikipedia.org/wiki/Looney_Tunes_and_Merrie_Melodies_filmography_(1950-59)",
-                 "https://en.wikipedia.org/wiki/Looney_Tunes_and_Merrie_Melodies_filmography_(1960-69)",
-                 "https://en.wikipedia.org/wiki/Looney_Tunes_and_Merrie_Melodies_filmography_(1970-present_and_miscellaneous)"
+               "https://en.wikipedia.org/wiki/Looney_Tunes_and_Merrie_Melodies_filmography_(1940-49)",
+               "https://en.wikipedia.org/wiki/Looney_Tunes_and_Merrie_Melodies_filmography_(1950-59)",
+               "https://en.wikipedia.org/wiki/Looney_Tunes_and_Merrie_Melodies_filmography_(1960-69)",
+               "https://en.wikipedia.org/wiki/Looney_Tunes_and_Merrie_Melodies_filmography_(1970-present_and_miscellaneous)"
 ]
 
 everyEpisode = []
 
+'''
 with urllib.request.urlopen(wikipediaPages[0]) as url:
-    a = url.read
+  a = url.read
+'''
+a = requests.get(wikipediaPages[0]).text
 wikiSoup = BeautifulSoup(a,"lxml")
 everyEpisode.extend(parseOneWikipediaPageSoup(wikiSoup))
+
+pythonJsonWriter(everyEpisode)
+
+
